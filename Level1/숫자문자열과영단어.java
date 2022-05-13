c157e6427463ed7cdd349a2a8acd6e3f626c1b0c
Young eun Choi <duddms0190@naver.com>
2022-05-13 23:47:13 +0900

    1단계

---
+++ Level1/숫자문자열과영단어.java
@@ -0,0 +1,115 @@
+package Level1;
+
+
+public class 숫자문자열과영단어 {
+
+    public static void main(String[] args) {
+        // 프로그래머스 코딩테스트 연습
+        // Lv1. 숫자 문자열과 영단어
+
+
+        숫자문자열과영단어.Solution s = new 숫자문자열과영단어.Solution();
+
+        System.out.println("결과: "+s.solution("one4seveneight"));
+        System.out.println("기대: 1478");
+        System.out.println();
+        System.out.println("결과: "+s.solution("23four5six7"));
+        System.out.println("기대: 234567");
+        System.out.println();
+        System.out.println("결과: "+s.solution("2three45sixseven"));
+        System.out.println("기대: 234567");
+        System.out.println();
+        System.out.println("결과: "+s.solution("123"));
+        System.out.println("기대: 123");
+        System.out.println();
+    }
+
+    static class Solution {
+        public int solution(String s) {
+            int answer = 0;
+
+            System.out.println(s);
+            while(s.length() > 0) {
+                answer *= 10;
+
+                int num = isNumber(s.substring(0,1));
+                if(num == -1) {
+                    num = str2num(s.substring(0, 2));
+                    s = s.substring(sublength(num));
+                }
+                else {
+                    s = s.substring(1);
+                }
+
+                answer += num;
+                System.out.println(answer);
+                System.out.println(s);
+            }
+
+
+            /*
+            // 진짜 똑똑하다....
+            String[] alphabets = {"zero","one","two","three","four","five","six","seven","eight","nine"};
+
+            for(int i=0; i<10; i++){
+                s = s.replaceAll(alphabets[i], i);
+            }
+            return Integer.parseInt(s);
+             */
+
+            return answer;
+        }
+
+        private int isNumber(String s) {
+            switch (s) {
+                case "0": return 0;
+                case "1": return 1;
+                case "2": return 2;
+                case "3": return 3;
+                case "4": return 4;
+                case "5": return 5;
+                case "6": return 6;
+                case "7": return 7;
+                case "8": return 8;
+                case "9": return 9;
+                default: return -1;
+            }
+        }
+
+        private int str2num(String s) {
+            switch (s) {
+                case "ze": return 0;
+                case "on": return 1;
+                case "tw": return 2;
+                case "th": return 3;
+                case "fo": return 4;
+                case "fi": return 5;
+                case "si": return 6;
+                case "se": return 7;
+                case "ei": return 8;
+                case "ni": return 9;
+                default: return -1;
+            }
+        }
+
+        private int sublength(int num) {
+            switch (num) {
+                case 0:
+                case 4:
+                case 5:
+                case 9:
+                    return 4;
+                case 1:
+                case 2:
+                case 6:
+                    return 3;
+                case 3:
+                case 7:
+                case 8:
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Level1/없는숫자더하기.java
@@ -0,0 +1,55 @@
+package Level1;
+
+import java.util.ArrayList;
+
+public class 없는숫자더하기 {
+
+    public static void main(String[] args) {
+        // 프로그래머스 코딩테스트 연습
+        // Lv1. 숫자 문자열과 영단어
+
+
+        없는숫자더하기.Solution s = new 없는숫자더하기.Solution();
+
+        int[] numlist = new int[]{1,2,3,4,6,7,8,0};
+        System.out.println("결과: "+s.solution(numlist));
+        System.out.println("기대: 14");
+        System.out.println();
+
+
+        numlist = new int[]{5, 8, 4, 0, 6, 7, 9};
+        System.out.println("결과: "+s.solution(numlist));
+        System.out.println("기대: 6");
+        System.out.println();
+    }
+
+    static class Solution {
+        public int solution(int[] numbers) {
+
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int i = 0; i < 10; i++)
+                list.add(i);
+
+            for(int num : numbers) {
+                System.out.print(num+" ");
+                list.remove((Integer) num);
+            }
+
+            int answer = 0;
+            for(int num: list)
+                answer += num;
+
+            return answer;
+
+
+            /*
+            int sum = 45;
+            for (int i : numbers) {
+                sum -= i;
+            }
+            return sum;
+             */
+
+        }
+    }
+}
