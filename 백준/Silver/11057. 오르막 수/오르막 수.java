657b2697161704ecd06779342a32e22500b34b6e
euuuunii <69569809+YoungEun-Choi930@users.noreply.github.com>
2023-02-22 11:38:41 +0900

    [Silver I] Title: 오르막 수, Time: 128 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// 입력파일 객체화
+		int N = Integer.parseInt(br.readLine());
+
+		// solve
+		int[] curNumCnt = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+		int[] nextNumCnt = null;
+		for(int n = 1 ; n < N ; n++) {
+			
+			nextNumCnt = new int[10];
+			// 0부터 9까지 앞자리 숫자.
+			int cnt = 0;
+			for(int i = 0 ; i < 10; i++) {
+				cnt += curNumCnt[i]; // 앞자리가 n인 경우가 몇개 인지.
+				nextNumCnt[i] += cnt;
+				nextNumCnt[i] %= 10_007;
+			}
+			curNumCnt = Arrays.copyOf(nextNumCnt, 10);
+		}
+		
+		int ans = 0;
+		for(int n : curNumCnt) {
+			//System.out.print(n+" ");
+			ans += n;
+			ans %= 10_007;
+		}
+		//System.out.println();
+
+		// 출력
+		System.out.println(ans);
+	}
+}
