fda293f05697c9b4bc3343285b07c38ee0520ea6
euuuunii <69569809+YoungEun-Choi930@users.noreply.github.com>
2023-04-02 01:03:29 +0900

    [Silver I] Title: 피아노 체조, Time: 580 ms, Memory: 73400 KB -BaekjoonHub

--- 백준/Silver/21318. 피아노 체조/피아노 체조.java
+++ 백준/Silver/21318. 피아노 체조/피아노 체조.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 
@@ -13,16 +10,16 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		String[] input = br.readLine().split(" ");
 
-		List<Integer> miss = new ArrayList<>();
+		int[] list = new int[N+1];
 		int pre = Integer.parseInt(input[0]);
 		int cur;
 		for(int i = 1 ; i < N; i++) {
 			cur = Integer.parseInt(input[i]);
-			if(pre > cur) miss.add(i);
+			if(pre > cur) list[i] = list[i-1] + 1;
+			else list[i] = list[i-1];
 			pre = cur;
 		}
-		// 배열로
-		int[] arr = miss.stream().mapToInt(Integer::intValue).toArray();
+		list[N] = list[N-1];
 		
 		// Question
 		int Q = Integer.parseInt(br.readLine());
@@ -33,12 +30,7 @@ public class Main {
 			int start = Integer.parseInt(input[0]);
 			int end = Integer.parseInt(input[1]);
 			
-			int count = 0;
-			int find = Arrays.binarySearch(arr, start);
-			if(find < 0) find = find * -1 -1;
-			for(int idx = find ; idx < arr.length && arr[idx] < end; idx++) {
-				count++;
-			}
+			int count = list[end-1] - list[start-1];
 			
 			sb.append(count).append('\n');
 			
